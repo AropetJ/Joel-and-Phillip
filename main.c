79dbf7cfa4c0806e6dc668a412d0bf7e9a1e4ec2
phillip mugisawq <phillipmugisa@icloud.com>
2023-04-27 15:26:08 +0300

    refractor

--- main.c
+++ main.c
@@ -18,9 +18,6 @@ int main(__attribute((unused)) int ac, char **av)
 	size = 0;
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-			printf("#cisfun$ ");
-
 		read = getline(&buf, &size, stdin);
 		if (read == -1)
 		{
