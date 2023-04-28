fef860e375c22b99064269bbf67e02021e34cfd5
AropetJ <aropetjoel32@gmail.com>
2023-04-28 23:47:08 +0200

    complete task 1

--- main.c
+++ main.c
@@ -15,25 +15,21 @@ int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
 	ssize_t prompt;
 	size_t n = 0;
 	/* char *args; */
-	bool piped = false;
 	int term;
 
-	while (1 && !piped)
+	while (1)
 	{
 		term = isatty(STDIN_FILENO);
-		if (term == 0)
-		{
-			piped = true;
-		}
-
-		write(1, sh, 9);
+		if (term)
+			write(1, sh, 9);
 		prompt = getline(&buf, &n, stdin);
 		if (prompt == -1)
 		{
-			perror("ERROR: Getline failed");
-			free(buf);
-			exit(EXIT_FAILURE);
+			if (term && feof(stdin))
+				term = 0;
+			break;
 		}
+
 		if ((buf[prompt - 1] == '\n'))
 			(buf[prompt - 1]) = '\0';
 
